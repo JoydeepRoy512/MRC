eaebdef4afdc1c225615d2175b570caf38c31124
JoydeepRoy512 <105724747+JoydeepRoy512@users.noreply.github.com>
2022-05-17 18:33:40 +0530

    Add files via upload

---
+++ js/owl.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    "user strict";
+    $(".testimonial").owlCarousel(
+        {
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            nav: true,
+            center: false,
+            autoplay: true,
+            autoplayTimeout:3000,
+            dots:true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 1,
+                    nav: false
+                },
+                1000: {
+                    items: 2,
+                    nav: true,
+                    loop: true,
+                    
+                }
+            }
+        }
+    );
+
+    $(".nav-item").click(function () {
+        // If the clicked element has the active class, remove the active class from EVERY .nav-link>.state element
+        if ($(this).hasClass("active")) {
+          $(".nav-item").removeClass("active");
+          $(this).addClass("active");
+        }
+        // Else, the element doesn't have the active class, so we remove it from every element before applying it to the element that was clicked
+        else {
+          $(".nav-item").removeClass("active");
+          $(this).addClass("active");
+        }
+      });
+
+})
