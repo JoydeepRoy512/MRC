7f66f43e7a8ab862943e1011daa7c137ff7d05df
JoydeepRoy512 <105724747+JoydeepRoy512@users.noreply.github.com>
2022-05-30 12:08:24 +0530

    Add files via upload

--- js/owl.js
+++ js/owl.js
@@ -27,19 +27,21 @@ $(document).ready(function () {
                 }
             }
         }
-    );
+    )
 
     $(".nav-item").click(function () {
         // If the clicked element has the active class, remove the active class from EVERY .nav-link>.state element
         if ($(this).hasClass("active")) {
-          $(".nav-item").removeClass("active");
           $(this).addClass("active");
+          
         }
         // Else, the element doesn't have the active class, so we remove it from every element before applying it to the element that was clicked
         else {
           $(".nav-item").removeClass("active");
           $(this).addClass("active");
         }
-      });
+        
+      })
 
 })
+
